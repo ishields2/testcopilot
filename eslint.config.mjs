43e1fa2a3007e7afbc451ac040ffa5f6a07b061f
ishields2 <ishields2@gmail.com>
2025-07-15 07:58:26 +0100

    ignore CLI from front end deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["testcopilot-cli/**/*"], // 👈 This is the key addition
+  },
 ];
 
 export default eslintConfig;
